a2ad3c6ea49626ea56265a47cc2d2c30a03ef080
Marko Malis <you@example.com>
2017-02-24 13:45:15 +0100

    MM: updates

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+app.get('/declan', function (req, res) {
+  res.sendFile(path.join(__dirname + '/declan.html'));
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 AFRAME.registerComponent('change-video', {
 	schema: {
 		on: {type: 'string', default: 'click'},
+		id: {type: 'string'},
 		target: {type: 'selector'},
 		src: {type: 'string'}
 	},
@@ -8,9 +9,38 @@ AFRAME.registerComponent('change-video', {
 		console.log('in init');
 		var data = this.data;
 		var el = this.el;
+		console.log(el.components.position);
 
 		el.addEventListener(data.on, function() {
 			console.log('di: ', el);
+			if(data.id==="#code") {
+				window.open('https://www.cameranu.nl/nl/p816015/kodak-pixpro-sp360-4k-action-cam-dual-pro-pack');
+			}
+			restartVideo(data.target.getAttribute('src'));
+			data.target.setAttribute('src', data.id);
+			data.target.setAttribute('material', 'src: ' + data.src);
+			el.emit('explode');
+		});
+	}
+});
+
+function restartVideo(id) {
+	let video = document.querySelector(id);
+	video.pause();
+	video.currentTime = 0;
+	video.load();
+}
+
+AFRAME.registerComponent('launch-to-invisible', {
+	schema: {
+		on: {type: 'string', default: 'click'}
+	},
+	init: function () {
+		console.log('in launch-to-invisible init');
+		var data = this.data;
+		var el = this.el;
+
+		el.addEventListener(data.on, function() {
 			data.target.setAttribute('src', data.src);
 			data.target.setAttribute('material', 'src: ' + data.src);
 		});
